40622b0740c6bf901fe1f0ab339c703d7fe91dc4
Pratyush Thota <=>
2023-02-07 19:25:47 -0700

    rremoved items directory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core'
 import { Routes } from '@angular/router'
 import { NativeScriptRouterModule } from '@nativescript/angular'
 
-import { ItemsComponent } from './item/items.component'
-import { ItemDetailComponent } from './item/item-detail.component'
-
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
